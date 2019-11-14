23a8f7911dc40dbf8cf87de4d6d8ecebc42ed51d
Daniel Gordon <d_gordon1@icloud.com>
2019-11-15 09:35:31 +1100

    fix redirection problem on destroy.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -42,7 +42,7 @@ class PostsController < ApplicationController
   # DELETE /posts/1
   def destroy
     @post.destroy
-    redirect_to posts_url, notice: 'Post was successfully destroyed.'
+    redirect_to root_path, notice: 'Post was successfully destroyed.'
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  root to: 'posts#index'
+  root to: 'posts#index' # -> if you have no root path your app will crash on heroku
   resources :posts, except: :index
 end
